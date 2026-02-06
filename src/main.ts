6c494d9e8d551ae98690e357843a3814571bcc53
maneesh kumar <maneeshk@unicourt.com>
2026-02-06 10:29:40 +0530

    doc:Enable global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      forbidUnknownValues: true,
+      stopAtFirstError: false,
+      validationError: {
+        target: false,
+        value: false,
+      },
+    }),
+  );
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
